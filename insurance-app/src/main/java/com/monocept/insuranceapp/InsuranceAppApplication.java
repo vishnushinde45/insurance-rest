a317e291b6decef42b2a6cc19fded5703e85c915
vishnushinde45 <vshinde13798@gmail.com>
2022-10-25 13:58:12 +0530

    first commit

---
+++ insurance-app/src/main/java/com/monocept/insuranceapp/InsuranceAppApplication.java
@@ -0,0 +1,13 @@
+package com.monocept.insuranceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceAppApplication.class, args);
+	}
+
+}
---
+++ insurance-app/src/test/java/com/monocept/insuranceapp/InsuranceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monocept.insuranceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
